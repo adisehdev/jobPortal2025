34968d3d68df224f549efdbefc6128199a5a9a06
adisehdev <sehdevaditya@gmail.com>
2025-07-28 22:28:01 +0530

    changes in postcss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,7 @@
-// postcss.config.js
-
-export default {
-  plugins: {
-    'tailwindcss': {},
-    'autoprefixer': {},
-  },
+const config = {
+  plugins: ["@tailwindcss/postcss"],
+     '@tailwindcss/postcss': {},
+  
 };
+
+export default config;
