34e7abb8c7169078212247c61d50863e203312c7
adisehdev <sehdevaditya@gmail.com>
2025-07-28 23:01:23 +0530

    simplifying db file

--- src/lib/dbConnection.js
+++ src/lib/dbConnection.js
@@ -7,13 +7,14 @@ export const connectDB = async () => {
 
     try {
         //console.log(dbString)
-        if(mongoose.connection.readyState === "1") { //if db is already connected
-            // 0 = disconnected, 1 = connected, 2 = connecting, 3 = disconnecting
+        // if(mongoose.connection.readyState === "1") { //if db is already connected
+        //     // 0 = disconnected, 1 = connected, 2 = connecting, 3 = disconnecting
             
-            console.log("db already connected");
-            return;
-        }
+        //     console.log("db already connected");
+        //     return;
+        // }
         const result  = await mongoose.connect(dbString);
+        return result;
         //console.log("db connected");
         
     } catch (error) {
