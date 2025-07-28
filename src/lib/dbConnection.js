1ba25a399f2000446993f3ade82922d42a8f7941
adisehdev <sehdevaditya@gmail.com>
2025-07-28 23:10:51 +0530

    changes in db file

--- src/lib/dbConnection.js
+++ src/lib/dbConnection.js
@@ -3,22 +3,22 @@ import mongoose from "mongoose";
 
 
 const connectDB = async () => {
-    const dbString = process.env.MONGO_URI
+    
 
     try {
         //console.log(dbString)
-        if(mongoose.connection.readyState === "1") { //if db is already connected
-            // 0 = disconnected, 1 = connected, 2 = connecting, 3 = disconnecting
+        // if(mongoose.connection.readyState === "1") { //if db is already connected
+        //     // 0 = disconnected, 1 = connected, 2 = connecting, 3 = disconnecting
             
-            console.log("db already connected");
-            return;
-        }
-        const result  = await mongoose.connect(dbString);
+        //     //console.log("db already connected");
+        //     return;
+        // }
+        const result  = await mongoose.connect(process.env.MONGO_URI);
         return result;
         //console.log("db connected");
         
     } catch (error) {
-        console.log(error);
+        //console.log(error);
         process.exit(1);
     }
 };
